617443e945c414aff2f16a21055fca3d69313e69
LeonardsonCC <leonardson.carvalho@gmail.com>
2021-08-04 09:29:20 -0300

    Upload file ok

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from flask import Flask, jsonify, request
 from rembg.bg import remove
 import numpy as np
 import io
@@ -5,8 +6,48 @@ from PIL import Image
 
 input_path = 'input.png'
 output_path = 'out.png'
+ALLOWED_EXTENSIONS = {'png'}
 
-f = np.fromfile(input_path)
-result = remove(f)
-img = Image.open(io.BytesIO(result)).convert("RGBA")
-img.save(output_path)
+app = Flask(__name__)
+
+print("Starting")
+def remove_background(stringFile):
+    f = np.fromstring(stringFile, dtype=np.uint8)
+    result = remove(f)
+    img = Image.open(io.BytesIO(result)).convert("RGBA")
+    img.save(output_path)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/")
+def route_index():
+    return jsonify({
+        "test": "ok"
+    })
+
+@app.route("/remove-background", methods=["POST"])
+def route_remove_background():
+    if 'file' not in request.files:
+        return jsonify({
+            "error": "No file received"
+        })
+    file = request.files['file']
+    
+    if file.filename == '':
+        return jsonify({
+            "error": "File not found"
+        })
+
+    if file and allowed_file(file.filename):
+        file_bytes = io.BytesIO()
+        file.save(file_bytes)
+        remove_background(file.getvalue())
+        return jsonify({
+            "data": "nice"
+        })
+
+    return jsonify({
+        "test": "nice"
+    })
